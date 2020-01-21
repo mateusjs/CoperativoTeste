1daba0dbc62c6105321361ccb8554d36e1e6029c
Mateus Jose dos Santos <mateus.santos@cotriguacu.com.br>
2020-01-21 17:02:57 -0300

    changing expandable details

--- lib/inicial.dart
+++ lib/inicial.dart
@@ -11,12 +11,26 @@ class InitialPage extends StatelessWidget {
       ),
       child: Center(
           child: RaisedButton(
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => HomePage()));
-        },
-        child: Text("Ração"),
-      )),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomePage()));
+              },
+              textColor: Colors.white,
+              padding: const EdgeInsets.all(0.0),
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: <Color>[
+                      Color(0xFF0D47A1),
+                      Color(0xFF1976D2),
+                      Color(0xFF42A5F5),
+                    ],
+                  ),
+                ),
+                padding: const EdgeInsets.all(10.0),
+                child: const Text('Abrir Dashboard',
+                    style: TextStyle(fontSize: 20)),
+              ))),
     );
   }
 }
--- lib/racao.dart
+++ lib/racao.dart
@@ -25,132 +25,150 @@ class _StatusPageState extends State<StatusPage> {
           margin: const EdgeInsets.all(8),
           height: 50,
           decoration: BoxDecoration(
-              border: Border.all(color: Colors.blue[400]),
+              border: Border.all(color: Colors.pink[300]),
+              color: Colors.pink[50],
               borderRadius: BorderRadius.all(Radius.circular(50))),
           child: Container(
             padding:
                 const EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
             child: Text('Suínos',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Color(0xFF000000), fontSize: 30)),
+                textAlign: TextAlign.center, style: TextStyle(fontSize: 30)),
             alignment: Alignment(0.0, 0.0),
           ),
         ),
-        collapsed: Container(
-          padding: const EdgeInsets.all(8),
-          decoration: const BoxDecoration(
-            border: Border(
-              top: BorderSide(width: 1.0, color: Color(0xFFFFDFDFDF)),
-              left: BorderSide(width: 1.0, color: Color(0xFFFFDFDFDF)),
-              right: BorderSide(width: 1.0, color: Color(0xFFFF7F7F7F)),
-              bottom: BorderSide(width: 1.0, color: Color(0xFFFF7F7F7F)),
+        collapsed: Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.95,
+            padding: const EdgeInsets.all(10),
+            decoration: const BoxDecoration(
+                border: Border(
+              top: BorderSide(width: 1.0, color: Colors.black12),
+              left: BorderSide(width: 1.0, color: Colors.black12),
+              right: BorderSide(width: 1.0, color: Colors.black12),
+              bottom: BorderSide(width: 1.0, color: Colors.black12),
+            )),
+            child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            CircularPercentIndicator(
+                              radius: 150.0,
+                              lineWidth: 10.0,
+                              percent: 1.0,
+                              animation: true,
+                              animationDuration: 1200,
+                              center: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    "100%",
+                                    style: TextStyle(fontSize: 40),
+                                  ),
+                                  Text("100Kg"),
+                                  Text("Ração recebida")
+                                ],
+                              ),
+                              progressColor: Colors.green,
+                            ),
+                          ],
+                        ),
+                        alignment: Alignment(0, 0),
+                      ),
+                      SizedBox(height: 10),
+                      Container(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            CircularPercentIndicator(
+                              radius: 150.0,
+                              lineWidth: 10.0,
+                              percent: 0.7,
+                              animation: true,
+                              animationDuration: 1200,
+                              center: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    "70%",
+                                    style: TextStyle(fontSize: 40),
+                                  ),
+                                  Text("700Kg"),
+                                  Text("Ração atual")
+                                ],
+                              ),
+                              progressColor: Colors.blue,
+                            ),
+                          ],
+                        ),
+                        alignment: Alignment(0, 0),
+                      ),
+                      SizedBox(height: 10),
+                      Container(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            CircularPercentIndicator(
+                              radius: 150.0,
+                              lineWidth: 10.0,
+                              percent: 0.0,
+                              animation: true,
+                              animationDuration: 1200,
+                              center: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    "0%",
+                                    style: TextStyle(fontSize: 40),
+                                  ),
+                                  Text("Em 30 dias!"),
+                                  Text("Dia 21/02")
+                                ],
+                              ),
+                              progressColor: Colors.green,
+                            ),
+                          ],
+                        ),
+                        alignment: Alignment(0, 0),
+                      ),
+                      SizedBox(height: 10),
+                      Center(
+                        child: RaisedButton(
+                            onPressed: () {},
+                            textColor: Colors.white,
+                            padding: const EdgeInsets.all(0.0),
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5)),
+                                gradient: LinearGradient(
+                                  colors: <Color>[
+                                    Color(0xFF0D47A1),
+                                    Color(0xFF1976D2),
+                                    Color(0xFF42A5F5),
+                                  ],
+                                ),
+                              ),
+                              padding: const EdgeInsets.all(10.0),
+                              child: const Text('Iniciar Conversa',
+                                  style: TextStyle(fontSize: 20)),
+                            )),
+                      )
+                    ],
+                  ),
+                );
+              },
             ),
-          ),
-          child: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              return Padding(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          CircularPercentIndicator(
-                            radius: 150.0,
-                            lineWidth: 10.0,
-                            percent: 1.0,
-                            animation: true,
-                            animationDuration: 1200,
-                            center: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text(
-                                  "100%",
-                                  style: TextStyle(fontSize: 40),
-                                ),
-                                Text("100Kg"),
-                                Text("Ração recebida")
-                              ],
-                            ),
-                            progressColor: Colors.green,
-                          ),
-                        ],
-                      ),
-                      alignment: Alignment(0, 0),
-                    ),
-                    SizedBox(height: 10),
-                    Container(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          CircularPercentIndicator(
-                            radius: 150.0,
-                            lineWidth: 10.0,
-                            percent: 0.7,
-                            animation: true,
-                            animationDuration: 1200,
-                            center: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text(
-                                  "70%",
-                                  style: TextStyle(fontSize: 40),
-                                ),
-                                Text("700Kg"),
-                                Text("Ração atual")
-                              ],
-                            ),
-                            progressColor: Colors.blue,
-                          ),
-                        ],
-                      ),
-                      alignment: Alignment(0, 0),
-                    ),
-                    SizedBox(height: 10),
-                    Container(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          CircularPercentIndicator(
-                            radius: 150.0,
-                            lineWidth: 10.0,
-                            percent: 0.0,
-                            animation: true,
-                            animationDuration: 1200,
-                            center: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text(
-                                  "0%",
-                                  style: TextStyle(fontSize: 40),
-                                ),
-                                Text("Em 30 dias!"),
-                                Text("Dia 21/02")
-                              ],
-                            ),
-                            progressColor: Colors.green,
-                          ),
-                        ],
-                      ),
-                      alignment: Alignment(0, 0),
-                    ),
-                    SizedBox(height: 10),
-                    Center(
-                      child: RaisedButton(
-                        child: Text("Iniciar uma conversa"),
-                        onPressed: () {},
-                      ),
-                    )
-                  ],
-                ),
-              );
-            },
           ),
         ),
         tapHeaderToExpand: true,
